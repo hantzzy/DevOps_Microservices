b81626ed5ab15262a1a2481b6652b5160797499e
hantzyy <hantalvarez@gmail.com>
2020-01-11 13:48:33 -0500

    edited all the files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=hantzy/prediction 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker pull hantzy/prediction:udacity_container_prediction3
+kubectl run --generator=run-pod/v1 udacity  --image=hantzy/prediction:udacity_container_prediction3  --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward  udacity 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=hantzy/prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag fc9c846eb990  hantzy/prediction:udacity_container_prediction3
 # Step 3:
 # Push image to a docker repository
+docker push hantzy/prediction
